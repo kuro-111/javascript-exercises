5bfa4aa93220cbf3b65511947162b21afeceed01
kuro-111 <graceyann666@gmail.com>
2023-06-02 09:02:37 +0800

    Solve 01,02,03 javascript exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
-};
+    if (num<0) {
+        return "ERROR"
+    }
+    
+      return (string.repeat(num))
+    
+
+}
+repeatString("hey",10);
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+ let str ='';
+
+ for (i= string.length-1; i>=0; i--){
+    str += string[i];
+ }
+    return str;
+
 
 };
 

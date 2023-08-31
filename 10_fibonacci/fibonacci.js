591aea7ae0a1b0e91b6a52cbc0176ca20a96d031
kuro-111 <graceyann666@gmail.com>
2023-09-01 07:15:19 +0800

    Finish Fibonacci, come back for the other two later

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (cycles) {
+     if (cycles < 0) return "OOPS";
+     if (cycles == 0) return 0;
+     if (cycles == 1) return 1;
+     let prevCycle = 0;
+     let currentTotal = 1;
+     for (var i = 2; i <= cycles; i++) {
+          let pastTotal = currentTotal;
+          currentTotal = prevCycle + currentTotal;
+          prevCycle = pastTotal;
+     }
+     return currentTotal;
 };
 
 // Do not edit below this line
